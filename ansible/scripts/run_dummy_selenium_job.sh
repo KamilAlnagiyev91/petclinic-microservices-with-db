ef2675fd614bd2285ef9160ab7535931fad11c93
KamilAlnagiyev91 <kamil.alnagiyev@gmail.com>
2025-05-26 20:02:22 +0000

    added qa automation pipeline for dev

---
+++ ansible/scripts/run_dummy_selenium_job.sh
@@ -0,0 +1,8 @@
+ansible-playbook --connection=local --inventory 127.0.0.1, --extra-vars "workspace=${WORKSPACE}" ./ansible/playbooks/pb_run_dummy_selenium_job.yaml
+```
+
+- Run the following command to test the `dummy_selenium_test_headless.py` file.
+
+```bash
+cd petclinic-microservices-with-db/
+ansible-playbook --connection=local --inventory 127.0.0.1, --extra-vars "workspace=$(pwd)" ./ansible/playbooks/pb_run_dummy_selenium_job.yaml
